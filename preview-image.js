c7c21d401a36daf113d3b08746fefc14594c4cd7
Maulana SK <134863162+Moleen@users.noreply.github.com>
2024-12-26 09:17:48 +0700

    Update preview-image.js

--- preview-image.js
+++ preview-image.js
@@ -1,27 +1,24 @@
 export class ImagePreview {
   constructor(element) {
     this.element = element;
-    this.target = $(element).attr('data-preview-target');
+    this.target = $(element).attr("data-preview-target");
     this.targetElement = $(`[data-preview=${this.target}]`);
-    this.currentImage = this.targetElement.attr('src');
+    this.currentImage = this.targetElement.attr("src");
     this.reader = new FileReader();
+    this.init();
   }
 
-  init(){
-    console.log('asdasd');
-    
+  init() {
     this.element.on("change", (event) => {
-        console.log('asdads');
-        const files = event.target.files;
-        if (files && files[0]) {
-          this.reader.onload = (e) => {
-           this.targetElement.attr("src", e.target.result);
-          };
-          this.reader.readAsDataURL(files[0]);
-        } else {
-            console.log(this.currentImage);
-         this.targetElement.attr("src", this.currentImage);
-        }
-      });
+      const files = event.target.files;
+      if (files && files[0]) {
+        this.reader.onload = (e) => {
+          this.targetElement.attr("src", e.target.result);
+        };
+        this.reader.readAsDataURL(files[0]);
+      } else {
+        this.targetElement.attr("src", this.currentImage);
+      }
+    });
   }
 }
